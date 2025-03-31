7292fb93f9a5746113fedbbc5363e20100ce1724
smacg <scg.contreras@gmail.com>
2025-03-30 23:14:48 -0300

    actualización

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 
 <template>
   <Contador></Contador>
+  <ListaDeTareas></ListaDeTareas>
 </template>
 
 <script setup>
 import Contador from './components/Contador.vue';
+import ListaDeTareas from './components/ListaDeTareas.vue';
 </script>
 
 
---
+++ src/components/ListaDeTareas.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>Lista de Tareas</h2>
+        <input type="text" v-model="nuevaTarea" @keyup.enter="addTarea">
+        <ul>
+            <li v-for="(item, index) in tareas" :key="index">
+                {{ item }}
+            </li>
+        </ul>
+    </div>
+
+
+
+</template>
+
+<script setup>
+import {ref} from 'vue';
+const tareas = ref([]);
+const nuevaTarea = ref('');
+const addTarea = () => {
+    tareas.value.push(nuevaTarea.value);
+    nuevaTarea.value = '';
+}
+
+
+
+</script>
+
+<style></style>
